6edec1463b9fc73130412bd37b2535773af3fc73
Abe Rubenstein <abe.rubenstein@datadoghq.com>
2021-02-28 17:51:06 -0500

    Move date filter to container and remove filter by stat

--- pages/stats.js
+++ pages/stats.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Container from 'components/container'
 import Seg from 'components/seg'
 import DateFilter from 'components/date-filter'
-import { useRouter } from 'next/router'
 import Head from 'next/head'
 
 import Table from 'components/table'
@@ -11,61 +10,15 @@ import { pretty } from 'lib/format'
 import { ItemIcon } from 'components/item-icon'
 const { SESSIONS } = require('lib/constants')
 
-const Stats = () => {
-  const router = useRouter()
+const Stats = ({ date, setDate }) => {
   const [value, setValue] = React.useState('')
   const handleChange = (e) => setValue(e.target.value)
   const [type, setType] = React.useState('all')
   const handleSelectStatType = (e) => setType(e.target.name)
-  const [date, setDate] = React.useState(router.query.date ?? 'all')
-  const [stat, setStat] = React.useState(router.query.stat ?? '')
+
   const [sort, setSort] = React.useState('')
   const [sortDir, setSortDir] = React.useState('desc')
-
-  const stats = React.useMemo(
-    () =>
-      (date !== 'all' ? buildStatsDiff : buildStats)(
-        date === 'all' ? SESSIONS[0] : date,
-      ),
-    [date],
-  )
-  const statTypes = React.useMemo(() => Object.keys(stats), [stats])
-
-  const statKeys = React.useMemo(() => {
-    const keys = {}
-
-    statTypes.forEach((statTypeKey) => {
-      Object.keys(stats[statTypeKey]).forEach((s) => {
-        keys[pretty(s)] = 1
-      })
-    })
-
-    return Object.keys(keys).sort((a, b) => (a > b ? 1 : -1))
-  }, [statTypes])
-
-  React.useEffect(() => {
-    let path = '/stats?'
-
-    if (date !== 'all') {
-      path += `&date=${date}`
-    }
-
-    if (stat) {
-      path += `&stat=${stat}`
-    }
-
-    router.replace(path)
-  }, [date, stat])
-
-  React.useEffect(() => {
-    if (router.query.date) {
-      setDate(router.query.date)
-    }
-
-    if (router.query.stat) {
-      setStat(router.query.stat)
-    }
-  }, [router.query.date, router.query.stat])
+  const stat = ''
 
   const prettyStat = pretty(stat)
 
@@ -79,56 +32,69 @@ const Stats = () => {
   }
 
   return (
-    <Container isPadded={false}>
-      <Head>
-        <title>
-          {stat ? `${prettyStat} - ` : ''}{' '}
-          {date ? `${date === 'all' ? 'All-time' : date} - ` : ''}JCA-MC
-        </title>
-      </Head>
-      <div className="sticky">
-        <input
-          className={value ? 'active' : ''}
-          type="search"
-          value={value}
-          placeholder="Search stats"
-          onChange={handleChange}
-        />
+    <Container isPadded={false} useDate>
+      {({ date, setDate }) => {
+        const stats = React.useMemo(
+          () =>
+            (date !== 'all' ? buildStatsDiff : buildStats)(
+              date === 'all' ? SESSIONS[0] : date,
+            ),
+          [date],
+        )
+        const statTypes = React.useMemo(() => Object.keys(stats), [stats])
 
-        <div className="inputs">
-          <DateFilter date={date} onChange={setDate} />
+        return (
+          <>
+            <Head>
+              <title>
+                {stat ? `${prettyStat} - ` : ''}{' '}
+                {date ? `${date === 'all' ? 'All-time' : date} - ` : ''}JCA-MC
+              </title>
+            </Head>
+            <div className="sticky">
+              <input
+                className={value ? 'active' : ''}
+                type="search"
+                value={value}
+                placeholder="Search stats"
+                onChange={handleChange}
+              />
 
-          {stat && (
-            <button className="stat-token" onClick={handleRemoveStat}>
-              <div>
-                <ItemIcon name={stat} />
-                {prettyStat}
+              <div className="inputs">
+                {stat && (
+                  <button className="stat-token" onClick={handleRemoveStat}>
+                    <div>
+                      <ItemIcon name={stat} />
+                      {prettyStat}
+                    </div>
+                  </button>
+                )}
               </div>
-            </button>
-          )}
-        </div>
 
-        <Seg
-          search={value}
-          stat={stat}
-          stats={stats}
-          type={type}
-          statTypes={statTypes}
-          onChange={handleSelectStatType}
-        />
-      </div>
+              <Seg
+                search={value}
+                stat={stat}
+                stats={stats}
+                type={type}
+                statTypes={statTypes}
+                onChange={handleSelectStatType}
+              />
+            </div>
 
-      <Table
-        type={type}
-        stats={stats}
-        statTypes={statTypes}
-        value={value}
-        currStat={stat}
-        isDiff={date !== 'all'}
-        handleSort={handleSort}
-        sort={sort}
-        sortDir={sortDir}
-      />
+            <Table
+              type={type}
+              stats={stats}
+              statTypes={statTypes}
+              value={value}
+              currStat={stat}
+              isDiff={date !== 'all'}
+              handleSort={handleSort}
+              sort={sort}
+              sortDir={sortDir}
+            />
+          </>
+        )
+      }}
     </Container>
   )
 }
